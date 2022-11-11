4a80695c9934aebeda596280281749de83f8f1d6
ziyaipek <ziya.ipek@hotmail.com>
2022-11-11 21:23:45 +0300

    Fourth_commit

---
+++ app_server/models/db.js
@@ -0,0 +1,26 @@
+var mongoose = require( 'mongoose' );
+var dbURI = 'mongodb+srv://ziya:mekanbul1234@mekanbul.slb9vco.mongodb.net/?retryWrites=true&w=majority'; 
+mongoose.connect(dbURI, {useNewUrlParser: true}); 
+mongoose.connection.on("connected", function() {
+    console.log(dbURI + "adresindeki veritabanına bağlanıldı!\n");
+});
+mongoose.connection.on("error", function(){
+    console.log("Bağlantı hatası\n");
+});
+mongoose.connection.on("disconnected", function() {
+    console.log("Bağlantı kesildi!\n");
+});
+
+function kapat(msg,callback) {
+ mongoose.connection.close(function() {
+    console.log(msg);
+    callback();
+ });
+}
+process.on("SIGNT", function() {
+    kapat("Uygulama kapatıldı!", function(){
+        process.exit(0);
+    });
+});
+
+require("./mekansema"); 
